aede9170b5d160dd7fd005b92f4858e9a3db6f81
Alexander Hiam <ahiam@marlboro.edu>
2013-02-27 20:54:03 -0500

    added support for the TI BQ32000

--- RTClib.cpp
+++ RTClib.cpp
@@ -21,6 +21,27 @@
 #define PCF8563_ADDRESS         0x51
 #define PCF8563_SEC_ADDR        0x02
 
+#define BQ32000_ADDRESS         0x68
+// BQ32000 register addresses:
+#define BQ32000_CAL_CFG1        0x07
+#define BQ32000_TCH2            0x08
+#define BQ32000_CFG2            0x09
+#define BQ32000_SFKEY1          0x20
+#define BQ32000_SFKEY2          0x21
+#define BQ32000_SFR             0x22
+// BQ32000 config bits:
+#define BQ32000__OUT            0x07 // CAL_CFG1 - IRQ active state
+#define BQ32000__FT             0x06 // CAL_CFG1 - IRQ square wave enable
+#define BQ32000__CAL_S          0x05 // CAL_CFG1 - Calibration sign
+#define BQ32000__TCH2_BIT       0x05 // TCH2 - Trickle charger switch 2
+#define BQ32000__TCFE           0x06 // CFG2 - Trickle FET control
+// BQ32000 config values:
+#define BQ32000_CHARGE_ENABLE   0x05 // CFG2 - Trickle charger switch 1 enable 
+#define BQ32000_SFKEY1_VAL      0x5E
+#define BQ32000_SFKEY2_VAL      0xC7
+#define BQ32000_FTF_1HZ         0x01
+#define BQ32000_FTF_512HZ       0x00
+
 #define SECONDS_PER_DAY         86400L
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -333,6 +354,134 @@ DateTime RTC_PCF8563::now() {
 
     return DateTime (y, m, d, hh, mm, ss);
 }
+
+
+///////////////////////////////////////////////////////////////////////////////
+// RTC_BQ32000 implementation
+
+void RTC_BQ32000::adjust(const DateTime& dt) {
+    Wire.beginTransmission(BQ32000_ADDRESS);
+    Wire.write((byte) 0);
+    Wire.write(bin2bcd(dt.second()));
+    Wire.write(bin2bcd(dt.minute()));
+    Wire.write(bin2bcd(dt.hour()));
+    Wire.write(bin2bcd(0));
+    Wire.write(bin2bcd(dt.day()));
+    Wire.write(bin2bcd(dt.month()));
+    Wire.write(bin2bcd(dt.year() - 2000));
+    Wire.endTransmission();
+}
+
+DateTime RTC_BQ32000::now() {
+    Wire.beginTransmission(BQ32000_ADDRESS);
+    Wire.write((byte) 0);
+    Wire.endTransmission();
+
+    Wire.requestFrom(DS1307_ADDRESS, 7);
+    uint8_t ss = bcd2bin(Wire.read());
+    uint8_t mm = bcd2bin(Wire.read());
+    uint8_t hh = bcd2bin(Wire.read());
+    Wire.read();
+    uint8_t d = bcd2bin(Wire.read());
+    uint8_t m = bcd2bin(Wire.read());
+    uint16_t y = bcd2bin(Wire.read()) + 2000;
+
+    return DateTime (y, m, d, hh, mm, ss);
+}
+
+void RTC_BQ32000::setIRQ(uint8_t state) {
+    /* Set IRQ square wave output state: 0=disabled, 1=1Hz, 2=512Hz. 
+     */
+  uint8_t reg, value;
+    if (state) {
+      // Setting the frequency is a bit complicated on the BQ32000:
+        Wire.beginTransmission(BQ32000_ADDRESS);
+	Wire.write(BQ32000_SFKEY1);
+	Wire.write(BQ32000_SFKEY1_VAL);
+	Wire.write(BQ32000_SFKEY2_VAL);
+	Wire.write((state == 1) ? BQ32000_FTF_1HZ : BQ32000_FTF_512HZ);
+	Wire.endTransmission();
+    }
+    value = readRegister(BQ32000_CAL_CFG1);
+    value = (!state) ? value & ~(1<<BQ32000__FT) : value | (1<<BQ32000__FT);
+    writeRegister(BQ32000_CAL_CFG1, value);
+}
+
+void RTC_BQ32000::setIRQLevel(uint8_t level) {
+    /* Set IRQ output level when IRQ square wave output is disabled to 
+     * LOW or HIGH. 
+     */
+    uint8_t value;
+    // The IRQ active level bit is in the same register as the calibration
+    // settings, so we preserve its current state:
+    value = readRegister(BQ32000_CAL_CFG1);
+    value = (!level) ? value & ~(1<<BQ32000__OUT) : value | (1<<BQ32000__OUT);
+    writeRegister(BQ32000_CAL_CFG1, value);
+}
+
+void RTC_BQ32000::setCalibration(int8_t value) {
+    /* Sets the calibration value to given value in the range -31 - 31, which
+     * corresponds to -126ppm - +63ppm; see table 13 in th BQ32000 datasheet.
+     */
+    uint8_t val;
+    if (value > 31) value = 31;
+    if (value < -31) value = -31;
+    val = (uint8_t) (value < 0) ? -value | (1<<BQ32000__CAL_S) : value;
+    val |= readRegister(BQ32000_CAL_CFG1) & ~0x3f;
+    Serial.println(val, BIN);
+    writeRegister(BQ32000_CAL_CFG1, val);
+}
+
+void RTC_BQ32000::setCharger(int state) {
+    /* If using a super capacitor instead of a battery for backup power, use this
+     * method to set the state of the trickle charger: 0=disabled, 1=low-voltage 
+     * charge, 2=high-voltage charge. In low-voltage charge mode, the super cap is 
+     * charged through a diode with a voltage drop of about 0.5V, so it will charge
+     * up to VCC-0.5V. In high-voltage charge mode the diode is bypassed and the super 
+     * cap will be charged up to VCC (make sure the charge voltage does not exceed your
+     * super cap's voltage rating!!). 
+     */
+    // First disable charger regardless of state (prevents it from 
+    // possible starting up in the high voltage mode when the low 
+    // voltage mode is requested):
+    uint8_t value;
+    writeRegister(BQ32000_TCH2, 0);
+    if (state <= 0 || state > 2) return;
+    value = BQ32000_CHARGE_ENABLE;
+    if (state == 2) {
+        // High voltage charge enable:
+        value |= (1 << BQ32000__TCFE);
+    }
+    writeRegister(BQ32000_CFG2, value);
+    // Now enable charger:
+    writeRegister(BQ32000_TCH2, 1 << BQ32000__TCH2_BIT);
+}
+
+
+uint8_t RTC_BQ32000::readRegister(uint8_t address) {
+    /* Read and return the value in the register at the given address. 
+     */
+    Wire.beginTransmission(BQ32000_ADDRESS);
+    Wire.write((byte) address);
+    Wire.endTransmission();
+    Wire.requestFrom(DS1307_ADDRESS, 1);
+    // Get register state:
+    return Wire.read();    
+}
+
+uint8_t RTC_BQ32000::writeRegister(uint8_t address, uint8_t value) {
+    /* Write the given value to the register at the given address. 
+     */
+    Wire.beginTransmission(BQ32000_ADDRESS);
+    Wire.write(address);
+    Wire.write(value);
+    Wire.endTransmission();
+}
+
+uint8_t RTC_BQ32000::isrunning() {
+    return !(readRegister(0x0)>>7);
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // RTC_Millis implementation
